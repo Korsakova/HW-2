20cdd03b790cceaaaf2e8a7f27e241f72f4a9a4e
Anastasiya <korsakova-nastena@yandex.ru>
2020-10-06 09:26:01 +0300

    Мой первый коммит)

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String[] errorWorld = new String[5];
+        errorWorld[3].length();
+        errorWorld[56563].length();
+
+
+    }
+}
