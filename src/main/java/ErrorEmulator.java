531ed32cd73e7e11dfda00723776fd950417d3ff
korsa <korsakova-nastena@yandex.ru>
2020-10-11 22:27:03 +0300

    третий коммит

--- src/main/java/ErrorEmulator.java
+++ src/main/java/ErrorEmulator.java
@@ -3,23 +3,39 @@ public class ErrorEmulator {
     public String iAmThrowsNullPointerException() throws NullPointerException {
         String iAmNeverReturn = "Я никогда не вернусь";
 
-        if (iAmNeverReturn != null) {
-            NullPointerException exception = new NullPointerException();
-            throw exception;
+
+        try {
+            if (iAmNeverReturn != null) {
+                NullPointerException exception = new NullPointerException();
+                throw exception;
+            }
+
+        }
+        catch(NullPointerException ex){
+            ex.printStackTrace();
+            iAmNeverReturn = iAmNeverReturn + " или вернусь!";
         }
 
         return iAmNeverReturn;
     }
 
-    public Integer iAmThrowsArrayIndexOutOfBoundsException( String[] massive ) throws ArrayIndexOutOfBoundsException{
-        // 1. определить длину массива (кол-во элементов в массиве)
-        int lengthMassive = massive.length;
-        // 2. прибавить к длинне массива 10
-        lengthMassive = lengthMassive + 10;
-        // 3. обратиться к ячейке массива с полученным значением
-        massive[lengthMassive];
-        // 4. вернуть длину строчки из полученной ячейки
+    public Integer iAmThrowsArrayIndexOutOfBoundsException(String[] massive) throws ArrayIndexOutOfBoundsException {
 
-        return null;
+       try{
+           // 1. определить длину массива (кол-во элементов в массиве)
+           int lengthMassive = massive.length;
+           // 2. прибавить к длинне массива 10
+           int valueY = lengthMassive + 10;
+           // 3. обратиться к ячейке массива с полученным значением
+           String valueZ = massive[valueY];
+       }
+
+       catch(ArrayIndexOutOfBoundsException ex2){
+           ex2.printStackTrace();
+           valueZ = valueZ + "нет ошибкам";
+       }
+
+        // 4. вернуть длину строчки из полученной ячейк
+        return valueZ.length();
     }
 }
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         ErrorEmulator emulator = new ErrorEmulator();
-
         System.out.println(emulator.iAmThrowsNullPointerException());
+
+        String[] newMassiva = {"ннн","ппп","жжж","ккк"};
+        System.out.println(emulator.iAmThrowsArrayIndexOutOfBoundsException(newMassiva));
     }
+
 }
+
